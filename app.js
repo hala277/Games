44e1308ac746ac56bb924317f1e7a0d3bc48619b
HalaAlmasharfeh <>
2021-07-14 17:25:11 +0000

    add js code

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+var name = prompt("what is your gamer name");
+alert("oh Hi nice name "+ name);
+var favGame = prompt("what is your fav game here?");
+
+if(favGame == "Call Of Duty"){
+  document.write("<div>" + "<h3" + favGame+"</h3> "+"</div>");
+}
+else if(favGame == "Minecraft" ){
+  document.write("<div>" + "<h3" + favGame+"</h3> "+"</div>");
+}
+else if(favGame == "Animal Crossing: New Horizon"){
+  document.write("<div>" + "<h3" + favGame+"</h3> "+"</div>");
+}
+else{
+  alert('we dont have this game');
+}
